4106f0e047a4ddf7de64299b2bb698871d7d874a
Sami Khan <samiwazir@gmail.com>
2026-08-28 12:47:38 +0500

    chore: cleanup scratch files

--- patch-vh.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('js/elevator-scene.js', 'utf8');
-
-// Replace visualViewport logic with a robust fallback
-code = code.replace(/window\.visualViewport \? window\.visualViewport\.height : window\.innerHeight/g, 
-"((window.visualViewport && window.visualViewport.height) ? window.visualViewport.height : window.innerHeight) || window.innerHeight || 800");
-
-fs.writeFileSync('js/elevator-scene.js', code);
-console.log("Patched visualViewport");
